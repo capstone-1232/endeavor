a0dab5d20174e6af941b2173ea8285ea31e9d40f
aaron.groot <potatopicker963@gmail.com>
2024-03-06 15:43:00 -0700

    trying to figure out git

--- home.php
+++ home.php
@@ -13,11 +13,46 @@
  */
 
 get_header();
+$title = get_the_title();
 ?>
-<h1>
-This is Beer And Coffee
-</h1>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <main>
+        <section>
+            <h2>Our Beer and Coffee</h2>
+            <div>
+                <a href="page-year-round.php">
+                <h3>Year-Round</h3>
+                    <img src="" alt="">
+                </a>
+                
+            </div>
+            <div>
+                <a href="page-seasonal.php">
+                <h3>Seasonal</h3>
+                    <img src="" alt="">
+                </a>
+                
+            </div>
+            <div>
+                <a href="page-barrel-aged.php">
+                <h3>Barrel Aged</h3>
+                    <img src="" alt="">
+                </a>
+                
+            </div>
+        </section>
+    </main>
+</body>
+</html>
 
 <?php
-
 get_footer();
+?>
