09e5f562e121c1f7cd23b0dbad82521a51443ade
aaron.groot <potatopicker963@gmail.com>
2024-04-03 12:44:24 -0600

    brianna styles

--- header.php
+++ header.php
@@ -28,45 +28,49 @@
 
 
 	<header class="main-header nav-flex">
-		<div class="xoo-wsc-cart-trigger">
-			<button class="shop-btn">
-				<svg fill="none" stroke="#f2f2f2" stroke-width="1.5" width="30px" height="28.13px" class="w-6 h-6"
-					viewBox="0 0 24 24">
-					<path stroke-linecap="round" stroke-linejoin="round"
-						d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-				</svg>
-			</button>
-		</div>
-
-		<div class="wide-nav-flex">
-			<div class="logo">
-				<?php
-				the_custom_logo();
-				?>
-				<h1 class="screen-reader-text">Endeavor Brewing Company</h1>
+		<div class="inner-container">
+			<div class="xoo-wsc-cart-trigger">
+				<button class="shop-btn">
+					<svg fill="none" stroke="#f2f2f2" stroke-width="1.5" width="30px" height="28.13px" class="w-6 h-6"
+						viewBox="0 0 24 24">
+						<path stroke-linecap="round" stroke-linejoin="round"
+							d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+					</svg>
+				</button>
 			</div>
 
+			<div class="wide-nav-flex">
+				<div class="logo">
+					<?php
+					the_custom_logo();
+					?>
+					<h1 class="screen-reader-text">Endeavor Brewing Company</h1>
+				</div>
 
 
-			<nav id="site-navigation" class="main-navigation ">
 
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id' => 'primary-menu',
-					)
-				);
-				?>
+				<nav id="site-navigation" class="main-navigation ">
 
-			</nav>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id' => 'primary-menu',
+						)
+					);
+					?>
+
+				</nav>
+			</div>
+
+			<button id="menu-toggle" class="menu-toggle shop-btn" aria-controls="primary-menu" aria-expanded="false">
+				<svg xmlns="http://www.w3.org/2000/svg" width="30px" height="28.13px" fill="none" viewBox="0 0 24 24"
+					stroke-width="1.5" stroke="#f2f2f2" class="w-6 h-6 site-navigation">
+					<path stroke-linecap="round" stroke-linejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+				</svg>
+			</button>
+
 		</div>
-
-		<button id="menu-toggle" class="menu-toggle shop-btn" aria-controls="primary-menu" aria-expanded="false">
-			<svg xmlns="http://www.w3.org/2000/svg" width="30px" height="28.13px" fill="none" viewBox="0 0 24 24"
-				stroke-width="1.5" stroke="#f2f2f2" class="w-6 h-6 site-navigation">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-			</svg>
-		</button>
 
 	</header>
