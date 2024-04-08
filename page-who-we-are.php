0f9aa4280b0363284569f5c7778c530fbce447ec
Brauunson <brauunsonhume@gmail.com>
2024-04-08 11:19:57 -0600

    Who we are basically complete

--- page-who-we-are.php
+++ page-who-we-are.php
@@ -76,12 +76,12 @@
         </section>
         <section>
             <div class="neo-inner-container">
-                <div class="flex">
+                <div class="flex last-flex">
                     <!-- This is for mobile -->
                     <div class="who-we-are-bg-img-3"></div>
     
                     <!-- This is for when we hit tablet view -->
-                    <img class="who-we-are-img" src="<?php echo get_template_directory_uri(); ?>/img/employee-holding-endeavour-glass.webp" alt="Employee holding endeavour glass">
+                    <img class="who-we-are-img beer-man" src="<?php echo get_template_directory_uri(); ?>/img/employee-holding-endeavour-glass.webp" alt="Employee holding endeavour glass">
                     <div class="mission-statement inner-container">
                         <h3>Mission Statement</h3>
                         <p>At Endeavour, we are more than just makers of great beer and coffee; we are cultivators of community, quality, and connection. Our mission is to create exceptional beer/coffee that not only tantalizes the taste buds but also brings people together in shared moments of joy and camaraderie. We are committed to upholding the highest standards of quality in every pint or cup of coffee we pour, ensuring that each sip is a testament to our dedication to excellence. Through our craft, we strive to create a welcoming space where individuals from all walks of life can gather, forge meaningful connections, and celebrate the simple pleasures of life over a pint of beer or a cup of coffee.</p>
